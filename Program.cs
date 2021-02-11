bcbe787e67b9d97eba919790b3e382ad9145cb76
Anastasiia Masiian <anastass413@gmail.com>
2021-02-11 11:49:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld2021
+{
+    class FirstLab
+    {
+        static void Main(string[] args)
+        {
+            int alpha = Convert.ToInt32(Console.ReadLine());
+
+            double z1 = (Math.Sin((Math.PI / 2) + 3 * alpha)) / (1 - Math.Sin(3 * alpha - Math.PI));
+            double z2 = Math.Cos((5 / 4) * Math.PI + (3 / 2 * alpha)) / Math.Sin((5 / 4) * Math.PI + (3 / 2 * alpha));
+
+            Console.WriteLine($"z1= {z1}, z2= {z2}");
+
+        }
+    }
+}
