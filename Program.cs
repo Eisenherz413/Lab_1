a4b51e4f0f5c0fd3f3a31f7cb5bd80407d002986
Anastasiia Masiian <anastass413@gmail.com>
2021-02-17 10:08:48 +0200

    Pishy

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace HelloWorld2021
         {
             int alpha = Convert.ToInt32(Console.ReadLine());
 
+
             double z1 = (Math.Sin((Math.PI / 2) + 3 * alpha)) / (1 - Math.Sin(3 * alpha - Math.PI));
             double z2 = Math.Cos((5 / 4) * Math.PI + (3 / 2 * alpha)) / Math.Sin((5 / 4) * Math.PI + (3 / 2 * alpha));
 
